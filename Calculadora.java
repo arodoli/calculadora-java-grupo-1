d3e1b3d3630037a41135784aa3a338b76a1ccfc4
pablomil2000 <martinlopezpablo@gmail.com>
2025-09-29 12:28:35 +0200

    feat: Implementacion division

---
+++ Calculadora.java
@@ -0,0 +1,17 @@
+public class Calculadora {
+    /**
+     * Método Division con control de división por cero
+     * @param dividendo
+     * @param divisor
+     * @return float
+     */
+    float division(int dividendo, int divisor) {
+        if (divisor==0) {
+            System.out.println("No puedes dividir por 0");
+            return null;
+        }
+        float resultado = dividendo/divisor;
+
+        return resultado;
+    }
+}
